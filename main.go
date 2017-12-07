a510fc918fc234f65aa0e543b2039a4fcbe7c3b7
srs60 <srs60@abv.bg>
2017-12-07 13:41:40 +0200

    2

--- main.go
+++ main.go
@@ -1,6 +1,25 @@
 package main
 
 func main() {
+	links := []string {
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
+		"http://amazon.com",
+	}
+	for _, link := range links {
+		checkLink(link)
+	}
 
+}
 
+func checkLink(link string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "might be down")
+		return
+	}
+	
+	fmt.Println(link, "is up!")
 }

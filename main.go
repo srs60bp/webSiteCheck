bf3507fc8b439a261297578f69869b5653b759e4
srs60 <srs60@abv.bg>
2017-12-08 01:34:13 +0200

    3

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+		"fmt"
+		"time"
+	)
+
 func main() {
 	links := []string {
 		"http://google.com",
@@ -8,18 +13,37 @@ func main() {
 		"http://golang.org",
 		"http://amazon.com",
 	}
+	
+	c := make(chan string)
+	
 	for _, link := range links {
-		checkLink(link)
+		checkLink(link, c)
 	}
+	
+//	for i := 0;i < len(links); i++ {
+//		fmt.Println(<-c)	
+//	}
+//	for {
+//		go checkLink(<-c, c)
+//	}
+	for l := range c {
+		checkLink(l, c)
+	}
+
 
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
+	time.Sleep(5 * time.Seconds)
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down")
+//		c <- "Might be down I think"
+		c<-link
 		return
 	}
 	
 	fmt.Println(link, "is up!")
+//	c <- "Yep its up"
+	c<-link
 }
